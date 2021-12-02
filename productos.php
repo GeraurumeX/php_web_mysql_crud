d6c27eb24291a77e9ce0f80ea8cc5b6d13905516
Gerardo Agustin <gerardoagustin@hotmail.com>
2021-12-02 11:10:12 -0300

    Construcción del sitio web

---
+++ productos.php
@@ -0,0 +1,52 @@
+<?php include("template/cabecera.php"); ?>
+
+<div class="col-md-3">
+
+    <div class="card">
+        <img class="card-img-top" src="https://w3schools.com/bootstrap4/img_avatar1.png" alt="">
+        <div class="card-body">
+            <h4 class="card-title">Libro Java</h4>
+            <a name="" id="" class="btn btn-primary" href="#" role="button">Ver más</a>
+        </div>
+    </div>
+
+</div>
+
+<div class="col-md-3">
+
+    <div class="card">
+        <img class="card-img-top" src="https://w3schools.com/bootstrap4/img_avatar1.png" alt="">
+        <div class="card-body">
+            <h4 class="card-title">Libro Java</h4>
+            <a name="" id="" class="btn btn-primary" href="#" role="button">Ver más</a>
+        </div>
+    </div>
+
+</div>
+
+<div class="col-md-3">
+
+    <div class="card">
+        <img class="card-img-top" src="https://w3schools.com/bootstrap4/img_avatar1.png" alt="">
+        <div class="card-body">
+            <h4 class="card-title">Libro Java</h4>
+            <a name="" id="" class="btn btn-primary" href="#" role="button">Ver más</a>
+        </div>
+    </div>
+
+</div>
+
+<div class="col-md-3">
+
+    <div class="card">
+        <img class="card-img-top" src="https://w3schools.com/bootstrap4/img_avatar1.png" alt="">
+        <div class="card-body">
+            <h4 class="card-title">Libro Java</h4>
+            <a name="" id="" class="btn btn-primary" href="#" role="button">Ver más</a>
+        </div>
+    </div>
+
+</div>
+
+
+<?php include("template/pie.php"); ?>
